a93bb5a65a1a8dcd6298fb6b905736d913f8fb52
Juho Gävert <jgavert@cs.helsinki.fi>
2012-03-08 14:13:47 +0200

    Experimental smoothing algorithm maybe full of mistakes

--- src/cpp/image.cpp
+++ src/cpp/image.cpp
@@ -1,4 +1,5 @@
 #include "image.h"
+#include "pixel.h"
 #include <fstream>
 #include <iostream>
 #include <cassert>
@@ -36,14 +37,76 @@ Image Image::readPPM(const std::string& filename) {
   return i;
 }
 
+
+
 void Image::smoothen() {
   cout << "Smoothing <3" << endl;
 
   // Smoothening. doing smoothed values into a new m_data
-  //char[m_height*m_width] data;
-  //for (int i=0; i<m_width ; i++) {
-  //  for (int k=0; k<m_height; k++) {
 
+  Pixel[m_height*m_width] data;
+  for (int i=1; i<=m_width; i++) {
+    for (int k=1; k<=m_height; k++) {
+      Pixel pxl = m_data[i*k];
+      bool right = false;
+      bool left = false;
+      //Saman rivin viereiset pikselit
+      if (i+1 < m_width) {
+        pxl.r += (pxl.r - m_data[(i+1)*k-1].r) * (1/7);
+        pxl.g += (pxl.g - m_data[(i+1)*k-1].g) * (1/7);
+        pxl.b += (pxl.b - m_data[(i+1)*k-1].b) * (1/7);
+        right = true;
+      }
+      if (i-1 > 0)
+      {
+        pxl.r += (pxl.r - m_data[(i-1)*k-1].r) * (1/7);
+        pxl.g += (pxl.g - m_data[(i-1)*k-1].g) * (1/7);
+        pxl.b += (pxl.b - m_data[(i-1)*k-1].b) * (1/7);
+        left = true;
+      }
+      //Yläpikselit
+      if (k-1 > 0)
+      {
+        if (right)
+        {
+          pxl.r += (pxl.r - m_data[(i+1)*(k-1)-1].r) * (1/7);
+          pxl.g += (pxl.g - m_data[(i+1)*(k-1)-1].g) * (1/7);
+          pxl.b += (pxl.b - m_data[(i+1)*(k-1)-1].b) * (1/7);
+        }
 
+        pxl.r += (pxl.r - m_data[(i)*(k-1)-1].r) * (1/7);
+        pxl.g += (pxl.g - m_data[(i)*(k-1)-1].g) * (1/7);
+        pxl.b += (pxl.b - m_data[(i)*(k-1)-1].b) * (1/7);
+        if (left)
+        {
+          pxl.r += (pxl.r - m_data[(i-1)*(k-1)-1].r) * (1/7);
+          pxl.g += (pxl.g - m_data[(i-1)*(k-1)-1].g) * (1/7);
+          pxl.b += (pxl.b - m_data[(i-1)*(k-1)-1].b) * (1/7);
+        }
+      }
 
+      //Alapikselit
+      if (k+1 < m_height)
+      {
+        if (right)
+        {
+          pxl.r += (pxl.r - m_data[(i+1)*(k+1)-1].r) * (1/7);
+          pxl.g += (pxl.g - m_data[(i+1)*(k+1)-1].g) * (1/7);
+          pxl.b += (pxl.b - m_data[(i+1)*(k+1)-1].b) * (1/7);
+        }
+
+        pxl.r += (pxl.r - m_data[(i)*(k+1)-1].r) * (1/7);
+        pxl.g += (pxl.g - m_data[(i)*(k+1)-1].g) * (1/7);
+        pxl.b += (pxl.b - m_data[(i)*(k+1)-1].b) * (1/7);
+
+        if(left)
+        {
+          pxl.r += (pxl.r - m_data[(i-1)*(k+1)-1].r) * (1/7);
+          pxl.g += (pxl.g - m_data[(i-1)*(k+1)-1].g) * (1/7);
+          pxl.b += (pxl.b - m_data[(i-1)*(k+1)-1].b) * (1/7);
+        }
+      }
+    }
+  }
+  m_data = data;
 }
